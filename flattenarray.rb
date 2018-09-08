540cf4f02236a18731d5819e774c3d72f9257689
Ariel <ariel.l.reyes@gmail.com>
2018-09-09 09:01:57 +1200

    Changed variable name from temp to temparray in flattenarray

--- flattenarray.rb
+++ flattenarray.rb
@@ -3,17 +3,19 @@
 class FlattenArray
   attr_accessor :tempvals
 
+  #Method for flattening a jagged array
   def flatarray(tempvals)
     @tempvals = tempvals
     puts @tempvals.inspect
-	temp = @tempvals.join(",").split(",").map(&:to_i).inspect
-	puts temp
-	return temp
+	temparray = @tempvals.join(",").split(",").map(&:to_i).inspect
+	puts temparray 
+	return temparray 
   end
   
 end
 
 if __FILE__ == $0
   ca = FlattenArray.new
+  #Default parameter provided 
   ca.flatarray(temparray = [[1,2,[3]],4])
 end
